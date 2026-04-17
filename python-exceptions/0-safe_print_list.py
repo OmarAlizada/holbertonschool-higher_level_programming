9d961c3ba8386ffb842ab91a8c1d497023f027a7
OmarAlizada <omer.alizade57@gmail.com>
2026-04-17 18:41:53 +0000

    defrgt

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list."""
     count = 0
     for i in range(x):
         try:
@@ -8,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-    print("") 
+    print("")
     return count
