7dff3a77007fd8f88a9f984324658dce9ebe3860
OmarAlizada <omer.alizade57@gmail.com>
2026-04-21 17:17:52 +0000

    fix: handle singular argument case correctly

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-import sys 
+import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    count = len(argv)
+    # Skriptin adını saymırıq
+    count = len(sys.argv) - 1
 
-    if count == 0 :
+    if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 arguments:")
+        # Burada "s" hərfi yoxdur!
+        print("1 argument:")
     else:
+        # Burada "s" hərfi var
         print("{} arguments:".format(count))
 
-    for i, arg in enumerate(argv, 1):
-        print("{}: {}".format(i, arg))
+    # Arqumentləri birdən başlayaraq sıralayırıq
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
