107499125e9bbe2ea285d4c9ccc65de11d0d59ff
OmarAlizada <omer.alizade57@gmail.com>
2026-04-23 22:50:59 +0000

    feat: discover and print non-private names from compiled module

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main":
+    names = dir(hidden_4)
+    names.sort()
+
+    for name in names:
+        if not name.startswith("__"):
+            print("{}".format(name))
