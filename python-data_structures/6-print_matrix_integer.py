4d33758b756f0fe9f9e9cfa56138cc6f7c1d71e7
OmarAlizada <omer.alizade57@gmail.com>
2026-04-26 22:34:38 +0000

    feat: add function to print a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    """Tam ededlerden ibaret martisi cap edir"""
+    """Matrisi rəqəmlər arasında boşluq olmaqla çap edir."""
     for row in matrix:
         for i in range(len(row)):
-            if i <  len(row) - 1:
+            # Əgər bu, sətrin sonuncu elementi deyilsə, boşluqla çap et
+            if i < len(row) - 1:
                 print("{:d}".format(row[i]), end=" ")
             else:
+                # Sonuncu elementdən sonra boşluq qoyma
                 print("{:d}".format(row[i]), end="")
-            print()
+        # Bir sətir tam bitdikdən sonra yeni sətirə keç
+        print()
