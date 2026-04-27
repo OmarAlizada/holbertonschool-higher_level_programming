bf03f722a60d94f1930d67394e5686ac80b671eb
OmarAlizada <omer.alizade57@gmail.com>
2026-04-27 13:22:13 +0000

    feat: add function that returns length and first character of a string

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Metnin uzunlugunu ve ilk herfini qaytarir."""
+    length = len(sentence)
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    
+    return(length, first_char)
